824efc75d07a1d882aefac6624b86e5ffd82b820
amee1311 <ameevaghasiya1311@gmail.com>
2023-11-28 19:18:57 -0500

    remaining changes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openPopup(popUpId) {
+    document.getElementById(popUpId).style.display = "block";
+}
+
+function closePopup(popUpId) {
+    document.getElementById(popUpId).style.display = "none";
+}
